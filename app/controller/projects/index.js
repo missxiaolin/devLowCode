f70cdb480eb68fd4afdea26999045a0e361dd68b
xiaolin <462441355@qq.com>
2021-07-01 22:18:10 +0800

    * add

--- app/controller/projects/index.js
+++ app/controller/projects/index.js
@@ -12,6 +12,30 @@ class ProjectsController extends Controller {
         const query = { limit: toInt(ctx.query.limit), offset: toInt(ctx.query.offset) };
         ctx.body = await ctx.model.Projects.findAll(query);
     }
+
+    async query() {
+        const {
+            id,
+        } = this.ctx.params;
+        const where = {};
+        if (id) where.id = id;
+        const result = await this.ctx.model.Project.findAll({
+            where,
+            order: [
+                // 将转义 title 并针对有效方向列表进行降序排列
+                ['updatedAt', 'DESC'],
+            ]
+        })
+        result.forEach(project => {
+            project.pageConfig = JSON.parse(project.pageConfig)
+            project.gitConfig = JSON.parse(project.gitConfig)
+            project.releaseInfo = JSON.parse(project.releaseInfo)
+        })
+        this.ctx.body = {
+            success: true,
+            result
+        }
+    }
 }
 
 module.exports = ProjectsController;

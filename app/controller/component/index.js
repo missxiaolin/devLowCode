cfb6252435aaf0e64b470dd610652b222faeb948
xiaolin <462441355@qq.com>
2021-08-01 14:06:04 +0800

    *接口

--- app/controller/component/index.js
+++ app/controller/component/index.js
@@ -11,26 +11,6 @@ const formatWhereCase = (params) => {
 };
 
 class ComponentController extends Controller {
-    /**
-     * 查询
-     */
-    async index() {
-        const {
-            id,
-            gitUrl,
-        } = this.ctx.params;
-        const where = {};
-        if (id) where.id = id;
-        if (gitUrl) where.gitUrl = gitUrl;
-        const result = await this.ctx.model.Component.findAll({
-            where
-        });
-        this.ctx.body = {
-            success: true,
-            result
-        }
-    }
-
     async query() {
         const { id, gitUrl } = this.ctx.params;
 

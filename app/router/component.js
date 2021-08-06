12f729b2b35ffd6c11467c712d798429ba7efcd9
xiaolin <462441355@qq.com>
2021-08-06 21:59:06 +0800

    add

--- app/router/component.js
+++ app/router/component.js
@@ -6,7 +6,6 @@
 module.exports = app => {
     const { router, controller } = app;
     // 查询
-    router.get('/component/query', controller.component.index.index);
     router.post('/component/add', controller.component.index.addComponent);
     router.post('/component/update', controller.component.index.updateComponent);
     router.get('/component/query', controller.component.index.query);
